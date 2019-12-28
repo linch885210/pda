3eb23d207edcdcc352aa936bb3b57fc2dc5aee2d
lincunhe <lchjj521>
2019-12-28 14:04:58 +0800

    pda代码

---
+++ src/main/java/com/song/mybatistest/dao/UserMapper.java
@@ -0,0 +1,31 @@
+package com.song.mybatistest.dao;
+
+import com.song.mybatistest.bean.User;
+import com.song.mybatistest.bean.UserExample;
+import com.song.mybatistest.bean.UserKey;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(UserKey key);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(UserKey key);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
